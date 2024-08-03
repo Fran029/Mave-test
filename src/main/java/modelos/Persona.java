80c22cac128b787fa9bf86603f0354fbed6b78d5
Francisca Robles <flanna.jpg@gmail.com>
2024-08-02 23:07:41 -0400

    clases persona y servicio

---
+++ src/main/java/modelos/Persona.java
@@ -0,0 +1,16 @@
+package modelos;
+
+import lombok.*;
+
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Persona {
+    private String rut;
+    private String nombre;
+
+
+}
---
+++ src/main/java/servicios/ServiciosPersona.java
@@ -0,0 +1,42 @@
+package servicios;
+
+import modelos.Persona;
+import java.util.HashMap;
+import java.util.Map;
+public class ServiciosPersona {
+    private Map<String, String> personasDB = new HashMap<>();
+
+    public String crearPersona(Persona persona) {
+        if (persona != null) {
+            personasDB.put(persona.getRut(), persona.getNombre());
+            return "Creada";
+        } else {
+            return "No creada";
+        }
+    }
+
+    public String actualizarPersona(Persona persona) {
+        if (persona != null) {
+            personasDB.put(persona.getRut(), persona.getNombre());
+            return "Actualizada";
+        } else {
+            return "No actualizada";
+        }
+    }
+
+    public Map<String, String> listarPersonas() {
+        return personasDB;
+    }
+
+    public String eliminarPersona(Persona persona) {
+        if (persona != null) {
+            personasDB.remove(persona.getRut());
+            return "Eliminada";
+        } else {
+            return "No eliminada";
+        }
+    }
+}
+
+
+
